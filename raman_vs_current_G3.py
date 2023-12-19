78c23cc36e35ae1fd226129bb833f0daede853b4
DAWE-NKTP <118812176+DAWE-NKTP@users.noreply.github.com>
2023-12-19 10:21:30 +0100

    Formatting

--- raman_vs_current_G3.py
+++ raman_vs_current_G3.py
@@ -1,4 +1,4 @@
-#%%
+# %%
 import pyvisa as visa
 import numpy as np
 import sys
@@ -7,7 +7,8 @@ from instrument_connection.scripts import AQ6315
 from module_connection import module_connection as mc
 from module_connection import NKTP_DLL as nk
 import matplotlib
-matplotlib.use('TkAgg')
+
+matplotlib.use("TkAgg")
 import matplotlib.pyplot as plt
 import time
 
@@ -17,120 +18,126 @@ instruments = np.array(rm.list_resources())
 print(instruments)
 
 nk.openPorts(nk.getAllPorts(), 0, 0)
-portlist = nk.getAllPorts().split(',')
+portlist = nk.getAllPorts().split(",")
 for port in portlist:
     result, devList = nk.deviceGetAllTypes(port)
     if result == 0:
         comport = port
 print(port)
-osa_address_long = instruments[['GPIB' in x for x in instruments]]
-osa_address_short = osa_address_long[0].split('::')[1]
+osa_address_long = instruments[["GPIB" in x for x in instruments]]
+osa_address_short = osa_address_long[0].split("::")[1]
 osa = AQ6315.OSA(address=osa_address_short)
-#%%
-osa.sweep_mode = 'SGL'
-osa.trace = 'A'
+# %%
+osa.sweep_mode = "SGL"
+osa.trace = "A"
 osa.set_sample(1000)
 osa.set_span(950, 1200)
 osa.set_res(1)
-osa.set_sens('SHI1')
+osa.set_sens("SHI1")
 osa.set_average_times(1)
-#%%
+# %%
 booster = mc.boosterModule(comport, 5)
 currents = np.array([0.7, 1, 1.5, 2, 2.5, 3, 3.5, 4, 4.5, 5])
 booster.unlock()
-booster.register_write('22', 0)  # Set booster current to zero
+booster.register_write("22", 0)  # Set booster current to zero
 mainboard = mc.baseModule(comport, 15)
-mainboard.register_write('32', 2)  # Set interlock to 2 (interlock off)
-#%%
-serial = 'K0109470'
-base_path = os.path.join(os.getcwd(), 'data')
+mainboard.register_write("32", 2)  # Set interlock to 2 (interlock off)
+# %%
+serial = "K0109470"
+base_path = os.path.join(os.getcwd(), "data")
 
 # uncoil_0deg, uncoil_45deg, coil_0deg, coil_45deg
-extension = 'coil_45deg'
-# extension = 'uncoil_45deg'
-
+extension = "coil_45deg"
 save_path = os.path.join(base_path, extension, serial)
 if not os.path.isdir(save_path):
     os.mkdir(save_path)
-#%%
-plt.close('all')
-mainboard.register_write('30', 1)  # Turn seed emission on
+# %%
+mainboard.register_write("30", 1)  # Turn seed emission on
 plt.pause(2)
 osa.sweep()
-osa.save(os.path.join(save_path, 'seed'))
-fig = osa.plot_trace('A')
+osa.save(os.path.join(save_path, "seed"))
+fig = osa.plot_trace("A")
 ax = fig.get_axes()[0]
-ax.set_title('Seed')
+ax.set_title("Seed")
 ax.set_ylim([-90, 0])
 fig.show()
 
-mainboard.register_write('30', 2)  # Turn preamp emission on
+mainboard.register_write("30", 2)  # Turn preamp emission on
 plt.pause(2)
 osa.sweep()
-osa.save(os.path.join(save_path, 'seed_preamp'))
-fig = osa.plot_trace('A')
+osa.save(os.path.join(save_path, "seed_preamp"))
+fig = osa.plot_trace("A")
 ax = fig.get_axes()[0]
-ax.set_title('Seed + Preamp')
+ax.set_title("Seed + Preamp")
 ax.set_ylim([-90, 0])
 fig.show()
-#%%
+# %%
 plt.close()
+
+
 def handle_close(evt):
     global continue_measuring
-    print ('Closed Figure, cancelling loop')
+    print("Closed Figure, cancelling loop")
     continue_measuring = False
 
+
 if len(currents) <= 10:
-    cmap = plt.get_cmap('tab10')(np.arange(10, dtype=int))
+    cmap = plt.get_cmap("tab10")(np.arange(10, dtype=int))
 else:
-    cmap = plt.get_cmap('tab20')(np.arange(20, dtype=int))
+    cmap = plt.get_cmap("tab20")(np.arange(20, dtype=int))
 
 fig, ax = plt.subplots()
 fig.show()
-ax.set_xlabel('Wavelength [nm]')
-ax.set_ylabel('Power [dBm]')
+ax.set_xlabel("Wavelength [nm]")
+ax.set_ylabel("Power [dBm]")
 ax.set_ylim([-80, -10])
-fig.canvas.mpl_connect('close_event', handle_close)
+fig.canvas.mpl_connect("close_event", handle_close)
 
 continue_measuring = True
 finished = False
-booster.register_write('30', 2)
+booster.register_write("30", 2)  # Turn on booster emission
 for indx, curr in enumerate(currents):
     if continue_measuring:
-        ax.set_title('Now: ' + str(curr) + f' A [{indx/len(currents)*100:.1f}%]' )
-        j = (indx)/len(currents)
-        print("[%-20s] %d%%" % ('='*int(20*j), 100*j) +  '\t[' + str(int(curr*1000)) + ' mA]', end='\r')
+        ax.set_title("Now: " + str(curr) + f" A [{indx/len(currents)*100:.1f}%]")
+        j = (indx) / len(currents)
+        print(
+            "[%-20s] %d%%" % ("=" * int(20 * j), 100 * j)
+            + "\t["
+            + str(int(curr * 1000))
+            + " mA]",
+            end="\r",
+        )
         fig.canvas.draw()
-        booster.register_write('22', curr)  # Set booster current to curr
+        booster.register_write("22", curr)  # Set booster current to curr
         plt.pause(1)
-        read_curr = booster.register_read('1A')[0][0]  # Get the stage 2 current reading
+        read_curr = booster.register_read("1A")[0][0]  # Get the stage 2 current reading
         print(read_curr)
         osa.sweep()
-        wl, intensity = osa.get_trace('A')
-        ax.plot(wl, intensity, color=cmap[indx], label=str(curr)+'A')
-        ax.legend(prop={'size': 14}, loc='upper right')
+        wl, intensity = osa.get_trace("A")
+        ax.plot(wl, intensity, color=cmap[indx], label=str(curr) + "A")
+        ax.legend(prop={"size": 14}, loc="upper right")
         fig.canvas.draw()
-        osa.save(os.path.join(save_path, str(curr)+'A'))
-        if indx == len(currents)-1:
+        osa.save(os.path.join(save_path, str(curr) + "A"))
+        if indx == len(currents) - 1:
             finished = True
     else:
         break
 
     if finished:
-        ax.set_title(f'{serial}')
+        ax.set_title(f"{serial}")
         fig.canvas.draw()
         # print('\n')
         # input('Press any key to exit')
 
-ax.set_xlabel('Wavelength [nm]')
-ax.set_ylabel('Power [dBm]')
+ax.set_xlabel("Wavelength [nm]")
+ax.set_ylabel("Power [dBm]")
 # ax.set_title('Raman')
 # ax.legend([str(x) + 'A' for x in currents])
 
-booster.register_write('22', 0.7)  # Turn current down to 0.7 A
+booster.register_write("22", 0.7)  # Turn current down to 0.7 A
 time.sleep(1)
-booster.register_write('30', 0)  # Turn off booster emission
+booster.register_write("30", 0)  # Turn off booster emission
 time.sleep(1)
-mainboard.register_write('30', 1)  # Turn off preamp
+mainboard.register_write("30", 1)  # Turn off preamp
 time.sleep(1)
-mainboard.register_write('30', 0)  # Turn off seed
+mainboard.register_write("30", 0)  # Turn off seed
